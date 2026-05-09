5380fad61e6c1503cdd808c952a9ff875179caaf
Joanna <joannakiruba@gmail.com>
2026-05-09 14:49:36 +0530

    Implemented local storage caching system

--- js/app.js
+++ js/app.js
@@ -1,2 +1,31 @@
 // Main application entry point
 // Integrates location, services, offline handling, and UI
+
+import {
+  saveServicesToCache,
+  getValidCachedServices
+} from "./offline/cacheService.js";
+
+
+// Test data
+const testServices = [
+  {
+    name: "Apollo Hospital"
+  }
+];
+
+
+
+// STEP 1:
+// Uncomment this ONLY ONCE to create cache
+
+//saveServicesToCache(testServices);
+
+
+
+// STEP 2:
+// Keep this active
+
+const cached = getValidCachedServices();
+
+console.log("Cached Data:", cached);
