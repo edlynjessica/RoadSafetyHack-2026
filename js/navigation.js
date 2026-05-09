a8cd0f690466471e03dd9f947f93b718f17782be
Diya Elizabeth J <j.diya.elizabeth@gmail.com>
2026-05-09 19:41:36 +0530

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,22 @@
+function goToMedical() {
+    window.location.href = "medical.html";
+}
+
+function goToVehicle() {
+    window.location.href = "vehicle.html";
+}
+
+function goHome() {
+    window.location.href = "index.html";
+}
+
+function selectSituation(situation) {
+
+    // Store selected situation for integration
+    localStorage.setItem("selectedSituation", situation);
+
+    console.log("Selected Situation:", situation);
+
+    // Navigate to services page
+    window.location.href = "services.html";
+}
